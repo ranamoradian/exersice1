5999137844bc9905c135b26c51e08a18349c11cd
ranamoradian <148284212+ranamoradian@users.noreply.github.com>
2023-10-20 18:37:21 +0330

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+
+name = input("Eneter your name: ")
+family = input(" Enter your family: ")
+a = int(input("Enter the grade of the first lessons: "))
+b: int(input("Enter the grade of the second lessons: "))
+c = int(input("enter the grade of the third lessons: "))
+average = ((a+b+c) / 3)
+
+if average > + 17:
+    print(" great ")
+
+
+if average < 17:
+    print(" Good")
+
+if average < 12:
+    print(" fail ")
